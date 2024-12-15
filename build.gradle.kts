b156c7768af54c25da5291ab95c9b049ab484bcd
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2024-12-15 16:15:21 +0900

    init - 초기 세팅

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+group = "io.github.gamedatatools"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.github.javafaker:javafaker:1.0.2")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
