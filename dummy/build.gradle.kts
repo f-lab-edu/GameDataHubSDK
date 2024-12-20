2675393020a9ef21edf2207f5e9add08cbf48bf4
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2024-12-20 22:52:54 +0900

    UPDATE - 서브 모듈 gradle 설정 삭제

--- dummy/build.gradle.kts
+++ dummy/build.gradle.kts
@@ -1,22 +1,3 @@
-plugins {
-    kotlin("jvm")
-}
-
-group = "com.gamedatahub.sdk"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation("com.github.javafaker:javafaker:1.0.2")
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(17)
 }
