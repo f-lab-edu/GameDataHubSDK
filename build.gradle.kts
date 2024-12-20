116737f4fef0c4dbc0c16090ece0e5e65eef4aa2
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2024-12-20 22:46:57 +0900

    UPDATE - 서브 모듈에 필요한 gradle 설정 정리

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,20 +2,26 @@ plugins {
     kotlin("jvm") version "2.0.21"
 }
 
-group = "com.gamedatahub.sdk"
-version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+subprojects {
+    group = "com.gamedatahub.sdk"
+    version = "1.0-SNAPSHOT"
+
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+
+    dependencies {
+        testImplementation(kotlin("test"))
+    }
+    tasks.test {
+        useJUnitPlatform()
+    }
+
+    kotlin {
+        jvmToolchain(17)
+    }
+
+    repositories {
+        mavenCentral()
+    }
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(17)
-}
