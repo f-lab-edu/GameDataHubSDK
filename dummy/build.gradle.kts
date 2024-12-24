d130758b826760695629eccda8007064ba3efccf
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2024-12-24 14:47:56 +0900

    ADD - http, json, 역직렬화 라이브러리 의존성 추가

--- dummy/build.gradle.kts
+++ dummy/build.gradle.kts
@@ -1,3 +1,15 @@
 dependencies {
-    implementation("com.github.javafaker:javafaker:1.0.2")
+    val jacksonVersion: String by project
+
+    // HTTP client
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    // json
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // Serialize
+    implementation("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+
 }
