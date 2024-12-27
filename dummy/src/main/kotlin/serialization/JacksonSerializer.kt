0a40c66bab680ace615c3ac83c5b30a48fabd7e9
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2024-12-27 21:59:05 +0900

    FEAT - dummy에서 Jackson 이용한 역직렬화 적용

---
+++ dummy/src/main/kotlin/serialization/JacksonSerializer.kt
@@ -0,0 +1,20 @@
+package serialization
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.sharedsdk.serialization.JsonSerializer
+
+
+class JacksonSerializer<T>(
+    private val objectMapper: ObjectMapper = ObjectMapper().registerModule(KotlinModule()),
+    private val clazz: Class<T>
+) : JsonSerializer<T> {
+
+    override fun serialize(data: T): String {
+        return objectMapper.writeValueAsString(data)
+    }
+
+    override fun deserialize(jsonData: String): T {
+        return objectMapper.readValue(jsonData, clazz)
+    }
+}
