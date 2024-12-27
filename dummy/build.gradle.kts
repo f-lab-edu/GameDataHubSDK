6b7c6ff693a458c0192b2ff73e69d4efd83c5469
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2024-12-27 21:59:22 +0900

    FEAT - dummy에 필요한 모듈 및 테스트 의존성 설정

--- dummy/build.gradle.kts
+++ dummy/build.gradle.kts
@@ -1,6 +1,9 @@
 dependencies {
     val jacksonVersion: String by project
 
+    implementation(project(":jvm-sdk"))
+    implementation(project(":shared-sdk"))
+
     // HTTP client
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
@@ -12,4 +15,7 @@ dependencies {
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
 
+    // Testing
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("com.squareup.okhttp3:mockwebserver:5.0.0-alpha.11")
 }
