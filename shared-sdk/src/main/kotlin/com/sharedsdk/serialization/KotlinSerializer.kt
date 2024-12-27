bebc3792814e98801af69e56c9d9e04b84e53f6f
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2024-12-27 21:58:03 +0900

    FEAT - shared-sdk 기본으로 제공될 역/직렬화 기능 제공

---
+++ shared-sdk/src/main/kotlin/com/sharedsdk/serialization/KotlinSerializer.kt
@@ -0,0 +1,22 @@
+package com.sharedsdk.serialization
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.json.Json
+
+class KotlinSerializer<T>(
+    private val serializer: KSerializer<T>
+) : JsonSerializer<T> {
+
+    private val json = Json {
+        encodeDefaults = true
+        prettyPrint = true
+    }
+
+    override fun serialize(data: T): String {
+        return json.encodeToString(serializer, data)
+    }
+
+    override fun deserialize(jsonData: String): T {
+        return json.decodeFromString(serializer, jsonData)
+    }
+}
